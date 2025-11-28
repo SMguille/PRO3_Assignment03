6703f77a9c49b31834d64ef4a4cd6c0b2d17affb
Guillermo <355442@via.dk>
2025-11-28 23:30:54 +0100

    Assignment03 deleted grpc stuff

--- src/main/java/via/pro3/repositories/DBConnection.java
+++ src/main/java/via/pro3/repositories/DBConnection.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String DB_URL = "jdbc:postgresql://localhost:5432/slaughterhouse_db";
     private static final String DB_USER = "postgres";
-    private static final String DB_PASSWORD = "guille123";
+    private static final String DB_PASSWORD = "password";
 
     public static Connection openConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
